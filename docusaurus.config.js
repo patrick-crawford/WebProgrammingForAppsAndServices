4f6536a08e6a6b4f8aa3427d768663f0da55a749
Patrick Crawford <patrick.crawford@senecacollege.ca>
2023-09-28 16:34:03 -0400

    added organizagtionName

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,6 +12,7 @@ module.exports = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+  organizationName: 'SDDS',
   projectName: 'WebProgrammingForAppsAndServices',
 
   markdown: {
