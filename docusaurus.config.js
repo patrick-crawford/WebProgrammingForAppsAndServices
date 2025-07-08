284ca6523448a7b8057c5a7ef8df698fea1b1aec
Patrick Crawford <patrick.crawford@senecapolytechnic.ca>
2025-07-07 21:58:52 -0400

    update pdf link in footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ module.exports = {
               html: `<a href='#' id='pwa-button' class='footer__link-item' hidden>Install as an App</a>`,
             },
             {
-              html: `<a href='../Web-Programming-for-Apps-and-Services.pdf' id='download-pdf' class='footer__link-item' download='Web-Programming-for-Apps-and-Services.pdf'>Download Notes (PDF)</a>`,
+              html: `<a href='../pdf/WebProgrammingForAppsAndServices.pdf' id='download-pdf' class='footer__link-item' download='WebProgrammingForAppsAndServices.pdf'>Download Notes (PDF)</a>`,
             },
           ],
         },
